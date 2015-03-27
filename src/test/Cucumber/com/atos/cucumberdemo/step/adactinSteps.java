dfc26b47ce35cbccf5ce6b7d992ccc97612e1a08
Vincent Free <neromy_1@msn.com>
2015-03-27 16:03:02 +0100

    Adactin testgevallen en code toevoegen

---
+++ src/test/Cucumber/com/atos/cucumberdemo/step/adactinSteps.java
@@ -0,0 +1,20 @@
+package com.atos.cucumberdemo.step;
+
+
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by VincentFree on 27-3-2015.
+ */
+public class adactinSteps {
+
+}
